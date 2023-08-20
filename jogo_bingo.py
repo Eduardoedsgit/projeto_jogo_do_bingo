e0d337f341dc5c535e6da0aacd376b73bbda1dea
Eduardoedsgit <142338989+Eduardoedsgit@users.noreply.github.com>
2023-08-20 17:12:28 -0300

    Update jogo_bingo.py
    desenvolvendo o jogo do bingo

--- jogo_bingo.py
+++ jogo_bingo.py
@@ -8,5 +8,11 @@ Inicio projeto: 20/08/2023
 #cria uma lista de ítens para ser inserido
 cartela1 = list();
 #atribui valores na variável lista criada. append(acrescentar).
-cartela1.append(int(12, 3, 1, 5, 6));
+cartela1.append(int(12));
+cartela1.append(int(3));
+cartela1.append(int(1));
+cartela1.append(int(5));
+cartela1.append(int(6));
 
+print('cartela n°1');
+print('{} {} {} {} {}'.format(cartela1[0], cartela1[1], cartela1[2], cartela1[3], cartela1[4]));
