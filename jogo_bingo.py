e7f643a088ddb1095d8f4bea06cf898513a7cb81
Eduardoedsgit <142338989+Eduardoedsgit@users.noreply.github.com>
2023-08-20 16:48:58 -0300

    Create jogo_bingo.py
    jogo bingo em python

---
+++ jogo_bingo.py
@@ -0,0 +1,12 @@
+'''
+Projeto Jogo do Bingo
+Autor: Eduardo Machado de Souza
+Inicio projeto: 20/08/2023
+
+'''
+
+#cria uma lista de ítens para ser inserido
+cartela1 = list();
+#atribui valores na variável lista criada. append(acrescentar).
+cartela1.append(int(12, 3, 1, 5, 6));
+
