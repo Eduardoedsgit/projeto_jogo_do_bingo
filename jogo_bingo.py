cc147aa928585a49bac0de179233ac5f3eb5804b
Eduardoedsgit <142338989+Eduardoedsgit@users.noreply.github.com>
2023-08-20 16:39:48 -0300

    Add files via upload

---
+++ jogo_bingo.py
@@ -0,0 +1,11 @@
+'''
+Projeto Jogo do Bingo
+Autor: Eduardo Machado de Souza
+Inicio projeto: 20/08/2023
+
+'''
+
+#cria uma lista de ítens para ser inserido
+cartela1 = list();
+#atribui valores na variável lista criada. append(acrescentar).
+cartela1.append();
